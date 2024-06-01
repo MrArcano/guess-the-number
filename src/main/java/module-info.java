326bd06f434116d877c192fbb89310274f6ecd4a
MrArcano <salvatore-dario@hotmail.it>
2024-06-02 00:10:03 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.exercises.guessthenumber {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.exercises.guessthenumber to javafx.fxml;
+    exports com.exercises.guessthenumber;
+}
