8d755e8a56c2fc1eebbbbb64cf6e8a7ba2cbf7a7
MrArcano <salvatore-dario@hotmail.it>
2024-06-02 12:53:13 +0200

    [add]: logic game, controll input, output info

--- src/main/java/com/exercises/guessthenumber/MainController.java
+++ src/main/java/com/exercises/guessthenumber/MainController.java
@@ -5,12 +5,12 @@
 package com.exercises.guessthenumber;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class MainController {
 
@@ -26,29 +26,131 @@ public class MainController {
     @FXML // fx:id="btnStartGame"
     private Button btnStartGame; // Value injected by FXMLLoader
 
+    @FXML // fx:id="errorNMax"
+    private Label errorNMax; // Value injected by FXMLLoader
+
+    @FXML // fx:id="errorNTest"
+    private Label errorNTest; // Value injected by FXMLLoader
+
+    @FXML // fx:id="errorTMax"
+    private Label errorTMax; // Value injected by FXMLLoader
+
     @FXML // fx:id="lblT"
     private Label lblT; // Value injected by FXMLLoader
 
     @FXML // fx:id="nMax"
     private TextField nMax; // Value injected by FXMLLoader
 
-    @FXML // fx:id="nTest"
-    private TextField nTest; // Value injected by FXMLLoader
-
     @FXML // fx:id="tMax"
     private TextField tMax; // Value injected by FXMLLoader
 
-    @FXML
-    void checkNumber(ActionEvent event) {
+    @FXML // fx:id="nTest"
+    private TextField nTest; // Value injected by FXMLLoader
 
-    }
+    @FXML // fx:id="messageArea"
+    private TextArea messageArea; // Value injected by FXMLLoader
+
+    @FXML // fx:id="pBarT"
+    private ProgressBar pBarT; // Value injected by FXMLLoader
+
+    private int maxAttempts;
+    private int remaingAttempts;
+    private int usedAttempts;
+    private int maxNumber;
+    private int checkNumber;
+    private int nRand;
 
     @FXML
     void startGame(ActionEvent event) {
+        // resetto i campi
+        messageArea.setText("");
+        nTest.setText("");
+        usedAttempts = 0;
+        errorNMax.setText("");
+        errorTMax.setText("");
+        errorNTest.setText("");
+        pBarT.setProgress(0);
+
+        // Prendo il numero massimo
+        try {
+            maxNumber = Integer.parseInt(nMax.getText());
+        } catch (NumberFormatException e) {
+            errorNMax.setText("Errore inserimento!");
+            return;
+        }
+
+        // Prendo il numero massimo di tentativi
+        try {
+            maxAttempts = Integer.parseInt(tMax.getText());
+            remaingAttempts = maxAttempts;
+        } catch (NumberFormatException e) {
+            errorTMax.setText("Errore inserimento!");
+            return;
+        }
+
+        // Genero un valore random tra 1 e nMax
+        Random random = new Random();
+        nRand = random.nextInt(maxNumber) + 1;
+        System.out.println(nRand); //debug
+
+        // setto i tentativi possibili
+        lblT.setText("Tentativi rimanenti: " + remaingAttempts);
+    }
+
+    @FXML
+    void checkNumber(ActionEvent event) {
+        // resetto la scritta di errore
+        errorNTest.setText("");
+
+        // se dovessi riprovare dopo che nn hai tentativi nn fare nulla
+        if (remaingAttempts == 0 || checkNumber == nRand) return;
+
+        // controllo che venga inserito un numero valido
+        try {
+            // controllo ripetizione numero
+            if(checkNumber == Integer.parseInt(nTest.getText())){
+                errorNTest.setText("Errore ripetizione!");
+                return;
+            }
+            checkNumber = Integer.parseInt(nTest.getText());
+        } catch (NumberFormatException e) {
+            errorNTest.setText("Errore inserimento!");
+            return;
+        }
+
+        // a questo punto posso contare i tentativi usati
+        usedAttempts++;
+        pBarT.setProgress((double) usedAttempts / maxAttempts);
+
+        // resetto il numero inserito
+        nTest.setText("");
+
+        // controllo se maggiore o minore
+        if (checkNumber > nRand) {
+            messageArea.appendText("T"+ usedAttempts  +": Il numero " + checkNumber + " è troppo ALTO!\n");
+        }
+        if (checkNumber < nRand) {
+            messageArea.appendText("T"+ usedAttempts  +": Il numero " + checkNumber + " è troppo BASSO!\n");
+        }
+
+        // aggiorno i tentativi rimanenti
+        remaingAttempts--;
+        lblT.setText("Tentativi rimanenti: " + remaingAttempts);
+
+        // controllo se hai vinto o perso
+        if (checkNumber == nRand) {
+            messageArea.appendText("Hai vinto. Il numero era: " + nRand + "\n");
+            pBarT.setProgress(1);
+            lblT.setText("Tentativi finiti");
+        } else if (remaingAttempts == 0) {
+            messageArea.appendText("Hai perso. Il numero era: " + nRand + "\n");
+            lblT.setText("Tentativi finiti");
+        }
 
     }
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+        // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
         assert btnCheck != null : "fx:id=\"btnCheck\" was not injected: check your FXML file 'main-view.fxml'.";
         assert btnStartGame != null : "fx:id=\"btnStartGame\" was not injected: check your FXML file 'main-view.fxml'.";
